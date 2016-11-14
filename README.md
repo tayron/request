ce257ff6901178b9531a700e9514604c2b60ef82
Tayon Miranda <dev@tayron.com.br>
2016-11-14 16:30:54 -0200

    UPDATE 	- Adicionando código fonte

---
+++ README.md
@@ -0,0 +1,23 @@
+## Request 1.0
+
+Classe que trata e gerencia informações de requisições
+
+
+## Recursos
+  - redirect($controllerAction) - Rerediona para a uri informada, exemplo: cliente/listar
+  - getUri() - Método que retorna uma lista de parametros informados na URI
+  - getPostParameter($key) - Metodo que retorna os parametros enviados via POST
+  - getGetParameter($key) - Metodo que retorna os parametros enviados via GET 
+  - url($controllerAction) - Método que monta a retorna url para uma determinada pagina
+   
+
+
+## Utilização via composer
+
+```sh
+    "require": {
+        ...
+        "tayron/request" : "1.0"
+        ... 
+    },    
+```
---
+++ Request.php
@@ -0,0 +1,215 @@
+<?php
+namespace OnFramework\lib;
+
+use \InvalidArgumentException;
+use \Exception;
+
+/**
+ * Classe que trata e gerencia informações de requisições
+ *
+ * @author Tayron Miranda <dev@tayron.com.br>
+ */
+final class Request 
+{	
+    /**
+     * Armazena a url base do servidor, exemplo: http://localhost ou http://localhost:81
+     * @var string
+     */
+    private $urlPath = null;
+    
+    /**
+     * Armazena a url base do camiho onde o sistema está, exemplo: / ou /diretorio/matriz/
+     * @var string
+     */    
+    private $urlBase = null;
+    
+    /**
+     * Armazena a url corrente, exemplo http://diretorio/onframework/clients/gerar-relatorio
+     * @var string
+     */        
+    private $urlCurrent = null;
+    
+    /**
+     * @var Singleton reference to singleton instance
+     */
+    private static $instance;
+
+    /**
+	 * Request::__construct
+	 * 
+     * Impede com que o objeto seja instanciado
+     */
+    final private function __construct()
+    {
+    }
+
+    /**
+	 * Request::__clone
+	 * 
+     * Impede que a classe Requisição seja clonada
+     *
+     * @throws Execao Lança execção caso o usuário tente clonar este classe
+     *
+     * @return void
+     */
+    final public function __clone()
+    {
+        throw new Exception('A classe Request não pode ser clonada.');
+    }
+	
+    /**
+	 * Request::__wakeup
+	 * 
+     * Impede que a classe Requisição execute __wakeup
+     *
+     * @throws Execao Lança execção caso o usuário tente executar este método
+     *
+     * @return void
+     */
+    final public function __wakeup()
+    {
+        throw new Exception('A classe Request não pode executar __wakeup.');
+    }
+	
+	/**
+	 * Request::getInstancia
+	 * 
+	 * Retorna uma instância única de uma classe.
+	 *
+	 * @staticvar Singleton $instancia A instância única dessa classe.
+	 *
+	 * @return Singleton A Instância única.
+	 */
+	public static function getInstancia() 
+	{
+		if (!static::$instance) {
+			static::$instance = new static();
+		}
+
+        // Armazena a url base do servidor, exemplo: http://localhost ou http://localhost:81
+        self::$instance->urlPath = (((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS']!=='off') || $_SERVER['SERVER_PORT']==443) ? 'https://':'http://' ).$_SERVER['HTTP_HOST'];
+        
+        // Armazena a url base do camiho onde o sistema está, exemplo: / ou /diretorio/matriz/
+        self::$instance->urlBase = str_replace('index.php', null, $_SERVER['SCRIPT_NAME']);
+        
+        // Armazena a url corrente, exemplo http://diretorio/onframework/clients/gerar-relatorio
+        self::$instance->urlCurrent = URL_PATH . $_SERVER['REQUEST_URI'];
+
+		return self::$instance;
+	}
+	
+    /**
+     * Request::redirect
+     * 
+     * Método que redireciona para uma outra tela
+     * 
+     * @param array $controllerAction Lista com nome do controller e action
+     * @return void
+     */
+    public function redirect(array $controllerAction)
+    {
+        $controller = (isset($controllerAction['controller']) && $controllerAction['controller'] !== null)
+            ? $controllerAction['controller'] : null;
+        
+        $action = (isset($controllerAction['action']) && $controllerAction['action'] !== null)
+            ? $controllerAction['action'] : null;
+        
+        if($controller == null && $action == null){
+            throw new InvalidArgumentException('redirect', 
+                'Deve-se informar um controller e/ou uma action para se efetuar o redirect');
+        }
+        
+        if($controller != null && $action != null){
+            header("Location: /$controller/$action");    
+        }
+        
+        if($controller != null && $action == null){
+            header("Location: /$controller");    
+        }
+        
+        if($controller == null && $action != null){
+            header("Location: /$action");    
+        }        
+    } 
+	
+    /**
+     * Request::getUri
+     * 
+     * Método que retorna uma lista de parametros informados na URI
+     * 
+     * @return array Lista de parametros da requisições uri
+     */	
+	public function getUri()
+	{
+        $itenListUri = explode('/', str_replace($this->urlPath . $this->urlBase, null, $this->urlCurrent));        
+		
+		$itensList = array();
+		foreach($itenListUri as $item){
+			$parameters = explode('?', $item);
+			array_push($itensList, current($parameters));		
+		}
+
+        return array_filter($itensList);
+	}
+    
+    /**
+	 * Request::getPostParameter
+	 * 
+     * Metodo que retorna os parametros enviados via POST
+	 * 
+	 * @param string $key Nome do parametro a ser recuperado
+	 * @return mixed Valor aramazenado via POST
+     */
+    public function getPostParameter($key = null)
+    {
+        return $this->getParameters($key, INPUT_POST);
+    }
+    
+    /**
+	 * Request::getGetParameter
+	 * 
+     * Metodo que retorna os parametros enviados via GET
+	 * 
+	 * @param string $key Nome do parametro a ser recuperado
+	 * @return mixed Valor aramazenado via GET
+     */
+    public function getGetParameter($key = null)
+    {
+        return $this->getParameters($key, INPUT_GET);
+    }    
+    
+    /**
+	 * Request::getParametros
+	 * 
+     * Método que retorna lista de parametros, seja via post ou via get
+	 * 
+	 * @param string $key Nome do parametro POST ou GET
+	 * @param string $type Tipo de parametro, sendo 1 para GET e 0 para POST
+	 * @return mixed Valor aramazenado via POST ou GET
+     */
+    private function getParameters($key, $type)
+    {
+        $parameters = filter_input_array($type);
+
+        if($key && isset($parameters[$key])){
+            return $parameters[$key];
+        }
+        
+        if($key == null){
+            return $parameters;
+        }        
+    }
+    
+    /**
+	 * Request::url
+	 * 
+     * Método que monta a retorna url para uma determinada pagina
+	 * 
+	 * @param string $controllerAction Nome do controller + action podendo ter parametros via get
+	 * @return string Retorna url completa com o link informado
+     */
+    public function url($controllerAction = null)
+    {
+        return str_replace('//', '/', $this->urlPath . $this->urlBase . $link);
+    }
+}
